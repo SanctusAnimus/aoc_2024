1e0c8136f5c748983dc50a4d0a744d0aaf51eabc
SanctusAnimus <43399078+SanctusAnimus@users.noreply.github.com>
2024-12-03 20:18:59 +0200

    switched to re2

--- day_3.hpp
+++ day_3.hpp
@@ -3,7 +3,7 @@
 
 
 #include "solution.hpp"
-#include <regex>
+#include <re2/re2.h>
 
 class Day3 : public Solution<Day3> {
 public:
@@ -29,47 +29,44 @@ public:
 		long int p1 = 0;
 		long int p2 = 0;
 
-		auto regex_start = chrono::high_resolution_clock::now();
-
-		std::regex tokenRegex(R"(do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\))");
-		auto begin = std::sregex_iterator(source.begin(), source.end(), tokenRegex);
-		auto end = std::sregex_iterator();
-
-		auto regex_end = chrono::high_resolution_clock::now();
-		Duration_T time_resolving = regex_end - regex_start;
-		std::cout << "regex took " << time_resolving.count() << "ms" << std::endl;
-
 		bool p2_enabled = true;
 
-		auto loop_start = chrono::high_resolution_clock::now();
+		std::string token, num1, num2;
+		re2::StringPiece input_sp(source);
 
-		for (auto& it = begin; it != end; ++it) {
-			std::string token = it->str();
-			// std::cout << "Processing " << token << " at position " << it->position() << std::endl;
+		RE2 re("(do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\))");
 
-			if (token == "do()") p2_enabled = true;
-			else if (token == "don't()") p2_enabled = false;
-			else {
-				int parsed_left, parsed_right;
-				size_t position = it->position() + 4;
-
-				auto [left_ptr, ec] = std::from_chars(ptr + position, ptr + position + 4, parsed_left);
-				std::from_chars(left_ptr + 1, left_ptr + 4, parsed_right);
-				long int result = parsed_left * parsed_right;
-				// std::cout << "mul( " << parsed_left << " * " << parsed_right << " " << source[position] << std::endl;
-				p1 += result;
-
-				if (p2_enabled) p2 += result;
-			}
+		if (!re.ok()) {
+			std::cerr << "Invalid regex: " << re.error() << std::endl;
 		}
 
-		auto loop_end = chrono::high_resolution_clock::now();
-		Duration_T time_looping = loop_end - loop_start;
-		// std::cout << "loop took " << time_looping.count() << "ms" << std::endl;
+		while (!input_sp.empty()) {
+			size_t match_start = source.size() - input_sp.size(); // Get match start position
+
+			if (RE2::FindAndConsume(&input_sp, re, &token, &num1, &num2)) {
+				if (token == "do()") {
+					p2_enabled = true;
+				}
+				else if (token == "don't()") {
+					p2_enabled = false;
+				}
+				else {
+					int parsed_left, parsed_right;
+					std::from_chars(num1.data(), num1.data() + num1.size(), parsed_left);
+					std::from_chars(num2.data(), num2.data() + num2.size(), parsed_right);
+					long int result = parsed_left * parsed_right;
+					p1 += result;
+
+					if (p2_enabled) p2 += result;
+				}
+			}
+			else {
+				break;
+			}
+		}
 
 		return { p1, p2 };
 	}
 };
-
 
 #endif
