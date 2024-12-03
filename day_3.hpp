25950e9742222196101201f1c342ead65b4c46f2
SanctusAnimus <43399078+SanctusAnimus@users.noreply.github.com>
2024-12-03 20:40:30 +0200

    switched to no-regex

--- day_3.hpp
+++ day_3.hpp
@@ -3,7 +3,6 @@
 
 
 #include "solution.hpp"
-#include <re2/re2.h>
 
 class Day3 : public Solution<Day3> {
 public:
@@ -20,8 +19,6 @@ public:
 
 
 	Result_T _get_solutions(SolutionInput_T solution_input) {
-		size_t cursor = 0;
-
 		std::string& source = solution_input[0];
 		size_t size = source.size();
 		const char* ptr = source.data();
@@ -31,39 +28,57 @@ public:
 
 		bool p2_enabled = true;
 
-		std::string token, num1, num2;
-		re2::StringPiece input_sp(source);
+		size_t cursor = 0;
 
-		RE2 re("(do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\))");
+		do {
+			size_t mul_pos = source.find("mul(", cursor);
+			size_t do_pos = source.find("do()", cursor);
+			size_t dont_pos = source.find("don't()", cursor);
 
-		if (!re.ok()) {
-			std::cerr << "Invalid regex: " << re.error() << std::endl;
-		}
+			if (mul_pos == std::string::npos) break;
 
-		while (!input_sp.empty()) {
-			size_t match_start = source.size() - input_sp.size(); // Get match start position
+			size_t closest_pos = std::numeric_limits<size_t>::max();
+			std::string closest_token;
 
-			if (RE2::FindAndConsume(&input_sp, re, &token, &num1, &num2)) {
-				if (token == "do()") {
-					p2_enabled = true;
-				}
-				else if (token == "don't()") {
-					p2_enabled = false;
-				}
+			if (mul_pos < closest_pos) {
+				closest_pos = mul_pos;
+				closest_token = "mul(";
+			}
+			if (do_pos != std::string::npos && do_pos < closest_pos) {
+				closest_pos = do_pos;
+				closest_token = "do()";
+			}
+			if (dont_pos != std::string::npos && dont_pos < closest_pos) {
+				closest_pos = dont_pos;
+				closest_token = "don't()";
+			}
+
+			cursor = closest_pos + 1;
+
+			if (closest_pos != std::numeric_limits<size_t>::max()) {
+				if (closest_token == "do()") p2_enabled = true;
+				else if (closest_token == "don't()") p2_enabled = false;
 				else {
-					int parsed_left, parsed_right;
-					std::from_chars(num1.data(), num1.data() + num1.size(), parsed_left);
-					std::from_chars(num2.data(), num2.data() + num2.size(), parsed_right);
-					long int result = parsed_left * parsed_right;
-					p1 += result;
+					// validate mul
+					closest_pos += 4; // step over "mul(" to first num
+					int num1 = 0, num2 = 0;
+					size_t comma_pos = source.find(',', closest_pos);
 
+					if ((comma_pos - closest_pos) > 4) continue;
+
+					size_t close_pos = source.find(')', comma_pos);
+
+					if ((close_pos - comma_pos) > 4) continue;
+
+					std::from_chars(ptr + closest_pos, ptr + comma_pos, num1);
+					std::from_chars(ptr + comma_pos + 1, ptr + close_pos, num2);
+
+					long int result = num1 * num2;
+					p1 += result;
 					if (p2_enabled) p2 += result;
 				}
 			}
-			else {
-				break;
-			}
-		}
+		} while(cursor < size);
 
 		return { p1, p2 };
 	}
